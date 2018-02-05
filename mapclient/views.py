3c0c08a728cde7c93be8cf53fcbd8b2a903924dd
Biplov Bhandari <bionicbiplov45@gmail.com>
2018-02-05 23:35:46 +0545

    bugfix for google oauth2client invalid_grant Error 400 (hopefully) :D

--- mapclient/views.py
+++ mapclient/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-
+import httplib2
 from oauth2client.contrib.django_util.decorators import oauth_required
 
 @oauth_required
 def index(request):
-    return render(request, 'map.html', {})
+
+    oauth = request.oauth
+    try:
+        oauth.credentials.get_access_token(httplib2.Http())
+        return render(request, 'map.html', {})
+    except Exception as e:
+        oauth.get_authorize_redirect()
