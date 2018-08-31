ab74714c8ba2a13f6c58776eef5c1c8aecfce872
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2018-08-31 17:00:48 +0500

    Change the field of shelter in group-by clause

--- mapclient/core.py
+++ mapclient/core.py
@@ -222,7 +222,7 @@ class GEEApi():
         warehouse_df = self.fc2df(self.TS_WH)
         shelter_df = self.fc2df(self.Shelter)
         shelter_df = shelter_df.drop(columns=['Latitude','Longitude'])
-        shelter_df = shelter_df.groupby(['Township']).sum()
+        shelter_df = shelter_df.groupby(['ID_3']).sum()
         water_percent_image = self._calculate_water_percent_image()
         empty = ee.Image().float()
         sumfeatures = water_percent_image.reduceRegions(
@@ -246,14 +246,14 @@ class GEEApi():
         df5 = pandas.merge(pop_wh_sh_df, flood_haz_df, how='outer', left_on='ID_3', right_on='ID_3')
         df5 = df5.fillna(0)
         df5['hazard'] = np.where(df5['Freclass']==1, 'Low', np.where(df5['Freclass']==2, 'Moderate', np.where(df5['Freclass']==3, 'High', 'None')))
-        # print("headers===",df5.columns.values)
+        print("headers===",df5.columns.values)
         return df5
 
 
     def getExposureData(self,request):
         exposure_df = self.getExposureTables()
         # exposure_df_wo_geo = exposure_df.drop(columns=['geometry'])
-        exposure_df_wo_geo = exposure_df[['NAME_0_x','NAME_1_x','NAME_2_x','NAME_3_x','Sum_Pop','no_warehou','hazard','No_shelter']]        
+        exposure_df_wo_geo = exposure_df[['FID_Cyclon','ID_3','NAME_0_x','NAME_1_x','NAME_2_x','NAME_3_x','Sum_Pop','no_warehou','hazard','No_shelter']]        
         json_data = exposure_df_wo_geo.to_json(orient='records')
         return json_data
 
